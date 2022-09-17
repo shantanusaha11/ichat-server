7ffca188cff34da125a2444a41670d4d3db09623
Shantanu Saha <shantanusaha11@gmail.com>
2022-09-17 15:05:33 +0530

    done!

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,17 @@
-import express from 'express';
-import { getAllUsers, login, register, setAvatar } from '../controllers/userController.js';
-import auth from '../middleware/auth.js';
+import express from "express";
+import {
+  getAllUsers,
+  login,
+  register,
+  setAvatar,
+} from "../controllers/userController.js";
+import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
-router.post("/register",register)
-router.post("/login",login)
-router.patch("/setAvatar/:id",auth,setAvatar);
-router.get("/users/:id",auth,getAllUsers);
+router.post("/register", register);
+router.post("/login", login);
+router.patch("/setAvatar/:id", auth, setAvatar);
+router.get("/users/:id", auth, getAllUsers);
 
 export default router;
